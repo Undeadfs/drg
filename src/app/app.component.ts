73b8f4f0cccd457edb6aa3281a33292feff3d1fa
Ruslan Gurbanov <impsermy@gmail.com>
2021-09-20 14:50:54 +0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drgndrp';
+
+  listOfItems = [
+    {
+      id:1,
+      isChecked: true,
+      name: 'show blue color'
+    },
+    {
+      id:2,
+      isChecked: true,
+      name: 'show purple color'
+    },
+    {
+      id:3,
+      isChecked: true,
+      name: 'show chocolate color'
+    },
+  ];
+
+  listOfBlocks = [
+    {
+      id: 1,
+      className: 'blue',
+      isShown: true
+    },
+    {
+      id: 2,
+      className: 'purple',
+      isShown: true
+    },
+    {
+      id: 3,
+      className: 'chocolate',
+      isShown: true
+    }
+  ];
+
+  changeBlockState(item: any) {
+    let selectedId = item.id;
+    let selectedBlock: any = this.listOfBlocks.find(item => item.id === selectedId);
+    
+    item.isChecked = !item.isChecked;
+    selectedBlock.isShown = item.isChecked;
+  }
 }
+
